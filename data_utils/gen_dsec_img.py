41713091c284b30f1352028ebf79dddee7e23b82
Abdessamad ELK <abdessamad.elkaouri@gmail.com>
2024-04-17 13:56:23 +0100

    Written gen_dsec_img.py

--- data_utils/gen_dsec_img.py
+++ data_utils/gen_dsec_img.py
@@ -1,55 +1,38 @@
-from event_slicer import EventSlicer
-import h5py
-import hdf5plugin
 import numpy as np
 import os
 from glob import glob
 from tqdm import tqdm
-import imageio
+import shutil
 
 if __name__ == '__main__':
     images_path = 'C:/users/abdessamad/DSEC_TMA/images'
-    event_path = 'C:/users/abdessamad/DSEC_TMA/train_events' 
     flow_path = 'C:/users/abdessamad/DSEC_TMA/train_optical_flow'
+    output_path = 'C:/users/abdessamad/DSEC_TMA_IMG'
+    sequences = os.listdir(images_path)
 
-    sequences = os.listdir(flow_path)
     for seq in sequences:
-        event_h5 = os.path.join(event_path, seq, 'events/left', 'events.h5')
-        rectify_h5 = os.path.join(event_path, seq, 'events/left', 'rectify_map.h5')
 
-        ts_file = os.path.join(flow_path, seq, 'flow', 'forward_timestamps.txt')
-        flow_dir = os.path.join(flow_path, seq, 'flow', 'forward') 
-        flow_list = sorted(glob(os.path.join(flow_dir, '*.png')))
-        timestamps = np.genfromtxt(ts_file, delimiter=',')
-        assert timestamps.shape[0]== len(flow_list)
-        
-        h5f = h5py.File(event_h5, 'r')
-        slicer = EventSlicer(h5f)
-        with h5py.File(rectify_h5, 'r') as h5_rect:
-            rectify_map = h5_rect['rectify_map'][()]
+        # Get image list and timestamps
+        images_dir = os.path.join(images_path, seq, 'images/left/rectified')
+        ts_file_img = os.path.join(images_path, seq, 'images', 'timestamps.txt')
+        timestamps_img = np.genfromtxt(ts_file_img, delimiter=',')
+        img_list = sorted(glob(os.path.join(images_dir, '*.png')))
+        assert timestamps_img.shape[0] == len(img_list)
 
-        output_dir = os.path.join('datasets/dsec_full/trainval', seq)
+        # Optical flow timestamps
+        ts_file_flow = os.path.join(flow_path, seq, 'flow', 'forward_timestamps.txt')
+        timestamps_flow = np.genfromtxt(ts_file_flow, delimiter=',')
+        timestamps_flow  = np.unique(timestamps_flow.flatten())
+
+        output_dir = os.path.join(output_path, seq)
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
-        for i in tqdm(range(len(timestamps)), ncols=60):
-            #current
-            t_curr = timestamps[i,0]
-            t_next = timestamps[i,1]
-            events_curr = slicer.get_events(t_curr, t_next)
-            if events_curr == None:
-                print(f'None data can be converted to voxel in {seq} at {i}th timestamps for current condition!')
-                continue
+        for i in tqdm(range(len(timestamps_flow)), ncols=60):
+            # Get image index
+            t_curr = timestamps_flow[i]
+            image_index = np.where(timestamps_img == t_curr)
 
-            # previous 
-            dt = 100 * 1000#us
-            t_prev = t_curr - dt
-            events_prev = slicer.get_events(t_prev, t_curr)
-            if events_prev == None:
-                print(f'None data can be converted to voxel in {seq} at {i}th timestamps for previous condition!')
-                continue
-
-
-            flow_16bit = imageio.imread(flow_list[i], format='PNG-FI')
-            np.save(os.path.join(output_dir, 'flow_{:06d}.npy'.format(i)), flow_16bit)
-        h5f.close()
+            # Copy image to the corresponding output directory
+            save_path = os.path.join(output_dir, '{:06d}.png'.format(i))
+            shutil.copy(img_list[i], save_path)
