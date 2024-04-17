dfdaa7b75396b92ed26f6f2d906d69cf52ddeaf0
Abdessamad ELK <abdessamad.elkaouri@gmail.com>
2024-04-17 14:08:11 +0100

    changed gen_dsec_img.py to generate from our existing DSEC_flow dataset

--- data_utils/gen_dsec_img.py
+++ data_utils/gen_dsec_img.py
@@ -5,22 +5,22 @@ from tqdm import tqdm
 import shutil
 
 if __name__ == '__main__':
-    images_path = 'C:/users/abdessamad/DSEC_TMA/images'
-    flow_path = 'C:/users/abdessamad/DSEC_TMA/train_optical_flow'
-    output_path = 'C:/users/abdessamad/DSEC_TMA_IMG'
+    images_path = 'D:/DSEC_flow/train'
+    flow_path = 'D:/DSEC_flow/train'
+    output_path = 'C:/users/abdessamad/TMA/datasets/dsec_full/trainval'
     sequences = os.listdir(images_path)
 
     for seq in sequences:
 
         # Get image list and timestamps
-        images_dir = os.path.join(images_path, seq, 'images/left/rectified')
-        ts_file_img = os.path.join(images_path, seq, 'images', 'timestamps.txt')
+        images_dir = os.path.join(images_path, seq, 'images_left/rectified')
+        ts_file_img = os.path.join(images_path, seq, 'images_timestamps.txt')
         timestamps_img = np.genfromtxt(ts_file_img, delimiter=',')
         img_list = sorted(glob(os.path.join(images_dir, '*.png')))
         assert timestamps_img.shape[0] == len(img_list)
 
         # Optical flow timestamps
-        ts_file_flow = os.path.join(flow_path, seq, 'flow', 'forward_timestamps.txt')
+        ts_file_flow = os.path.join(flow_path, seq, 'flow_forward_timestamps.txt')
         timestamps_flow = np.genfromtxt(ts_file_flow, delimiter=',')
         timestamps_flow  = np.unique(timestamps_flow.flatten())
 
@@ -34,5 +34,5 @@ if __name__ == '__main__':
             image_index = np.where(timestamps_img == t_curr)
 
             # Copy image to the corresponding output directory
-            save_path = os.path.join(output_dir, '{:06d}.png'.format(i))
+            save_path = os.path.join(output_dir, 'images', '{:06d}.png'.format(i))
             shutil.copy(img_list[i], save_path)
